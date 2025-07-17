8cac29b9ad2fe71448b2b39884f59658b0cf293b
chico <chicostudyat@gmail.com>
2025-07-17 19:51:39 +0530

    Added index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Student Management System</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	
+	<h1>Welcome to the student Management System</h1>
+    <div class="card-container">
+    	<div class="card">
+    		<h2>I am a student</h2>
+    		<a href="login/student_login.php">Login</a>
+    		<a href="register/student_register.php">Register</a>
+    	</div>
+    	<div class="card">
+    	    <h2>I am a teacher</h2>
+    	    <a href="login/teacher_login.php">Login</a>
+    	    <a href="register/teacher_register.php">Register</a>
+    	</div>
+    </div>
+
+</body>
+</html>
